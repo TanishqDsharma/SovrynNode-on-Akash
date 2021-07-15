aecc0f02f71b35004ad651938ab33f8bfc647b9f
Lightning Hero <43058749+TanishqDsharma@users.noreply.github.com>
2021-07-15 20:48:06 +0530

    Update accounts.js

--- secrets/accounts.js
+++ secrets/accounts.js
@@ -1,14 +1,14 @@
 export default {
     "liquidator": [{
-        adr: "0x95Af93a95D86115A1E73C6040c7703363B29a89F",
-        pKey: "3093ee0ee0ee1bf6b88bbbaa18ba7b7a3a5049ae3a1457e2cd4b714e99c277c5"
+        adr: "<liquidator_Account_address>",
+        pKey: "<liquidator_Account_private_key>"
     }],
     "rollover": [{
-        adr: "0xEeF9B9e162471553D1936afEE12a9C8fC7Bfb431",
-        pKey: "bafdcb29d82f428ab5f6ac50eeef961b9c320fb4b7232c1efc1a16c5971e0719"
+        adr: "<rollover_Account_address>",
+        pKey: "<rollover_Account_private_key>"
     }],
     "arbitrage": [{
-        adr: "0xd230D48ABB784C022687387596C85D1C2467Cb9d",
-        pKey: "c6b959d41eb9d2e4c4fa4e1226d61526f6fca33c8f0d8786627f91a92b0e8de4"
+        adr: "<arbitrage_Account_address>",
+        pKey: "<arbitrage_Account_private_key>"
     }],
 }
